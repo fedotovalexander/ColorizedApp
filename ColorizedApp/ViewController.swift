0cef9ca142a2876ab198cd1372a5cb434a54dc99
fedotovalexander <fedotov.sanek@gmail.com>
2024-12-08 18:09:30 +0300

    New app

--- ColorizedApp/ViewController.swift
+++ ColorizedApp/ViewController.swift
@@ -7,13 +7,54 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+final class ViewController: UIViewController {
 
+    @IBOutlet var colorsView: UIView!
+    
+    @IBOutlet var redSlider: UISlider!
+    @IBOutlet var greenSlider: UISlider!
+    @IBOutlet var blueSlider: UISlider!
+    
+    @IBOutlet var redLabel: UILabel!
+    @IBOutlet var greenLabel: UILabel!
+    @IBOutlet var blueLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        colorsView.layer.cornerRadius = 10
+        
+        updateColors()
+        
+        redLabel.text = String(format: "%.2f", redSlider.value)
+        greenLabel.text = String(format: "%.2f", greenSlider.value)
+        blueLabel.text = String(format: "%.2f", blueSlider.value)
+
     }
 
+    @IBAction func redSliderAction() {
 
+        updateColors()
+    }
+    
+    @IBAction func greenSliderAction() {
+
+        updateColors()
+    }
+    
+    @IBAction func blueSliderAction() {
+
+        updateColors()
+    }
+    
+    
+    private func updateColors() {
+        colorsView.backgroundColor = UIColor(
+             red: CGFloat(redSlider.value),
+             green: CGFloat(greenSlider.value),
+             blue: CGFloat(blueSlider.value),
+             alpha: 1
+             )
+    }
 }
 
